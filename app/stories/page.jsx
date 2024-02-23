9c8d59b522f7f4fa9b6c237f0b4801c3586e34b1
Kiritsu <deebh776@gmail.com>
2024-02-23 20:14:20 +0200

    add image story functionality

--- app/stories/page.jsx
+++ app/stories/page.jsx
@@ -1,4 +1,4 @@
-import Clientstory from "../components/clientside_story";
+import Clientstory from "@/components/clientside_story";
 
 // Icons
 import { FaFacebook } from "react-icons/fa";
@@ -9,77 +9,73 @@ import { CgMenuGridO } from "react-icons/cg";
 
 const Story = () => {
     return (
-        <div className="flex justify-between">
-            <div className="w-80 bg-white h-screen">
-                <span className="flex gap-2 shadow-md py-2 pl-3 w-full">
-                    <div className="group relative">
-                        <RxCross1 
-                          className="text-4xl cursor-pointer bg-slate-500 rounded-full p-2"
-                        />
-                        <span 
-                          className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
-                            Exit
-                        </span>
-                    </div>
-                    <FaFacebook 
-                      className="text-blue-600 text-4xl cursor-pointer"
-                    />
-                </span>
-
-                <div className="my-5 p-3">
-                    <span className="flex justify-between">
-                        <h2 
-                        className="font-bold text-2xl">
-                            Your story
-                        </h2>
-                        <IoMdSettings 
-                        className="text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2 cursor-pointer"
-                        />
-                    </span>
-
-                    <div className="flex items-center gap-3 mt-5">
-                        <IoPersonSharp 
-                        className="text-5xl rounded-full bg-gray-200 p-2"
-                        />
-                        <span className="font-medium">Guest</span>
-                    </div>
-                </div>
-                <hr />
+      <div className="flex justify-between">
+        <div className="w-80 bg-white h-screen">
+          <div className="flex gap-2 shadow-md py-2 pl-3 w-full">
+            <div className="group relative">
+              <RxCross1 
+                className="text-4xl cursor-pointer bg-gray-400 text-white rounded-full p-2"
+              />
+              <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
+                Exit
+              </span>
             </div>
+            <FaFacebook 
+              className="text-blue-600 text-4xl cursor-pointer"
+            />
+          </div>
 
-            <div className="flex h-9 gap-2 order-3 mr-5">
-                <div className="group relative cursor-pointer">
-                    <CgMenuGridO 
-                      className="text-black text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2"
-                    />
-                    <span 
-                      className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
-                        Menu
-                    </span>
-                </div>
+          <div className="my-5 p-3">
+            <span className="flex justify-between">
+              <h2 className="font-bold text-2xl">
+                Your story
+              </h2>
+              <IoMdSettings 
+                className="text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2 cursor-pointer"
+              />
+            </span>
 
-                <div className="group relative cursor-pointer">
-                    <IoNotifications 
-                      className="text-black text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2 cursor-pointer"
-                    />
-                    <span 
-                      className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
-                        Notifications
-                    </span>
-                </div>
-
-                <div className="group relative cursor-pointer">
-                    <IoPersonSharp 
-                      className="text-black text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2"
-                    />
-                    <span 
-                      className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
-                        Account
-                    </span>
-                </div>
+            <div className="flex items-center gap-3 mt-5">
+              <IoPersonSharp 
+                className="text-5xl rounded-full bg-gray-200 p-2"
+              />
+              <span className="font-medium">Guest</span>
             </div>
-          <Clientstory />
+          </div>
+          <hr />
         </div>
+
+          <div className="flex h-9 gap-2 order-3 mr-5">
+              <div className="group relative cursor-pointer">
+                  <CgMenuGridO 
+                    className="text-black text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2"
+                  />
+                  <span 
+                    className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
+                      Menu
+                  </span>
+              </div>
+
+            <div className="group relative cursor-pointer">
+              <IoNotifications 
+                className="text-black text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2 cursor-pointer"
+              />
+              <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
+                Notifications
+              </span>
+            </div>
+
+            <div className="group relative cursor-pointer">
+              <IoPersonSharp 
+                className="text-black text-4xl rounded-full bg-gray-200 hover:bg-gray-300 p-2"
+              />
+              <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
+                Account
+              </span>
+            </div>
+          </div>
+        <Clientstory />
+      </div>
     )
 }
 
--- components/clientside_story.jsx
+++ components/clientside_story.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link"
+import Image from "next/image"
 
 // Icons
 import { IoTextOutline } from "react-icons/io5";
@@ -9,35 +10,34 @@ import { MdAddPhotoAlternate } from "react-icons/md";
 
 const Clientstory = () => {
   // Variables
-  const [fileStory, setFilestory] = useState(null)
+  const [fileStory, setFilestory] = useState([])
+  const [selectedFile, setSelectedFile] = useState(null)
 
   // Functions
   const handleFile = (event) => {
-    setFilestory(event.target.files[0])
+    setSelectedFile(URL.createObjectURL(event.target.files[0]))
   }
-  if (fileStory) {
-    console.log("mission completed")
-  }
+
   return (
-    <div>
-      {fileStory ? (
-        <div className="flex justify-center items-center gap-5 ml-20">
+    <div className="flex justify-center items-center">
+      {!selectedFile ? (
+        <div className="flex gap-5 ml-20">
           <label 
             htmlFor="fileInput" 
             className="flex justify-center items-center h-80 w-52 bg-gradient-to-tr from-cyan-300 to-blue-900 rounded-lg hover:brightness-95">
             <input 
               id="fileInput" 
               type="file" 
+              onChange={handleFile}
               className="absolute inset-0 opacity-0 cursor-pointer" 
             />
-            <div className="flex-col items-center" onClick={handleFile}>
+            <div className="flex-col items-center">
               <MdAddPhotoAlternate 
                 className="rounded-full bg-white text-4xl p-1 mx-auto" 
               />
               <p className="text-white">Create a photo story</p>
             </div>
           </label>
-
           <div
             className="flex justify-center items-center h-80 w-52 cursor-pointer bg-gradient-to-t from-red-500 to-purple-600 rounded-lg hover:brightness-95">
             <div className="flex-col">
@@ -49,16 +49,24 @@ const Clientstory = () => {
           </div>
         </div>
         ) : (
-          <div className="bg-white shadow-md p-5 w-3/4 my-auto">
-            <div className="bg-black mb-5">
-              <div>
-                {fileStory}
-              </div>
+          <div className="bg-white shadow-md p-3 w-3/4 my-auto">
+          {/* <h2 className="font-semibold">Preview<h2/> */}
+          <div className="bg-black mb-5 flex justify-center items-center aspect-video">
+              <Image 
+                src={selectedFile} 
+                alt="Selected Photo" 
+                width={800}
+                height={450}
+              />
             </div>
 
-            <div className="m-2 border-2">
-              <Link href="" className="text-blue-500">Discard</Link>
-              <Link href="" className="text-blue-500">Create Story</Link>
+            <div className="m-2 flex gap-3 px-2">
+              <Link href="" className="bg-gray-300 px-2 font-medium w-28 flex justify-center items-center rounded-md">
+                Discard
+              </Link>
+              <Link href="" className="text-white bg-blue-500 p-2 w-28 font-medium flex justify-center items-center rounded-md">
+                Share to story
+              </Link>
             </div>
           </div>
         )
