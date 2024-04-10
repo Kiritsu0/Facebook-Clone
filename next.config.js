2e277fbe21e2c6d6eb03706b7565fe880c95b26a
Kiritsu <deebh776@gmail.com>
2024-04-10 14:08:28 +0300

    modify next config code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: "export",
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    basePath: '/Facebook-Clone',
+    assetPrefix: '/Facebook-Clone/',
+  };
